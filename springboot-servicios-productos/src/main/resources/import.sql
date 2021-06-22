1897f374d9634f4460da9553643b9742d12180d8
alex <alexjac49@gmail.com>
2021-06-23 01:11:32 +0200

    commit

---
+++ springboot-servicios-productos/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (name, price, create_at) VALUES ('tv Panasonic', 1500, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('cel Samsung A70', 500, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('PlayStation 5', 600, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Notebook Apple', 2500, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Reloj Xiaomi', 200, NOW());
