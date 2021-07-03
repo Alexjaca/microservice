df0eec9e16d248bf4d79a6d59bfada5d659bf3ae
alex <alexjac49@gmail.com>
2021-07-04 00:10:07 +0200

    CURDREPOSITORY

---
+++ springboot-servicios-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -0,0 +1,21 @@
+package com.formacionbdi.springboot.app.usuarios.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+
+@RepositoryRestResource(path = "usuarios")
+public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
+	
+	//http://localhost:8090/api/usuarios/usuarios/search/findByUsername?username=?  busqueda en postman
+	@RestResource(path = "buscar-username")
+	//http://localhost:8090/api/usuarios/usuarios/search/buscar-username?name=?  quedaria asi
+	public Usuario findByUsername(@Param("name") String username);
+	
+	@Query("select u from Usuario u where u.username=?1")
+	public Usuario obtenerPorUsername(String username);
+}
