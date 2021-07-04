6827eb81186dd3ec152f9a305b40bce29fbd3485
alex <alexjac49@gmail.com>
2021-07-04 16:52:21 +0200

    usuarios-commons

--- springboot-servicios-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServiciosUsuariosApplication.java
+++ springboot-servicios-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServiciosUsuariosApplication.java
@@ -2,7 +2,9 @@ package com.formacionbdi.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan({"com.formacionbdi.springboot.app.commons.usuarios.models.entity"}) // spring boot reconoce el package donde se ubican las entity
 @SpringBootApplication
 public class SpringbootServiciosUsuariosApplication {
 
